b93081989bd570f8bc248629206ba73bc81673a5
Justice Garner <justicegarner23@gmail.com>
2021-03-18 00:25:43 +0000

    Login/Signup

---
+++ login.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<?php ?>
+<body>
+<h1>The Fitness Witness</h1>
+<hr>
+<link href="style.css" rel="stylesheet" type="text/css" />
+<form action="login.php">  
+<div>    
+Name: <input name="input1" /><br>    
+Password: <input name="input2" /><br>    
+<input type="submit" name="add" value="Login" />  
+</div>
+</form>
+</body>
+</html>
